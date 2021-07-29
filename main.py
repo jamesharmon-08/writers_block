09469490cc1e549d1e87440d22469e48208d498b
jamesharmon <james.harmon@outlook.com>
2021-07-29 13:07:25 -0400

    Add autosave for every 100 characters and user initiated save (ESC)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ FONT = ('San Serif', 24)
 def current_milli_time():
     return round(time.time() * 1000)
 
+
 def change_text(delay):
     delay -= 1
     print(delay)
@@ -29,6 +30,9 @@ def onKeyPress(event):
     except TypeError:   # Non displayable key hit such as CMD, CTRL, Option ..
         print('Non displayable key hit')
         ascii=0
+    if ascii == 27:   #user hit ESC
+        save_info()
+        window.quit()
 
     if ascii >= 32:
         text.configure(state='normal')
@@ -43,11 +47,17 @@ def onKeyPress(event):
         text.configure(state='disabled')
         update_status()
 
+def save_info():
+    with open("writing.txt", mode='w') as f:
+        f.write(text.get("1.0", 'end'))
+
 
 def update_status():
-    char_count = len(list(text.get(1.0,'end'))) - 1
-    status_bar = Label(window, text=f"Characters: {char_count}", width=60, background='green', foreground='white', font=FONT).place(
-        x=10, y=765)
+    char_count = len(list(text.get(1.0,'end')))
+    if char_count % 100 == 0:  # save the last 100 characters to a file
+       save_info()
+    status_bar = Label(window, text=f"Characters: {char_count}", width=67, background='green', foreground='white', font=FONT).place(
+        x=0, y=765)
 
 
 
@@ -58,9 +68,10 @@ window = Tk()
 window.title("Writer's Block Assassin")
 window.geometry('1000x800')
 
-text = Text(window, width=60, background='White', foreground='black', font=FONT, wrap=WORD)
+text = Text(window, width=66, background='White', foreground='black', font=FONT, wrap=WORD)
 text.place(x=10, y=10)
 text.configure(state='disabled')
+text.tag_configure('where', underline=True, foreground='black')
 update_status()
 
 
